2a1fc87ef425c7420f745b9c5f0b4adf07870f44
Asslam Selimane <asslamselemane@gmail.com>
2026-05-27 18:16:50 +0200

    Add complete about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css'
 function App() {
   return (
     <div className="app">
+
+      {/* NAVBAR */}
+
       <header className="navbar">
         <h2 className="logo">AS</h2>
 
@@ -13,6 +16,8 @@ function App() {
           <a href="#">Contacto</a>
         </nav>
       </header>
+
+      {/* HERO SECTION */}
 
       <section className="hero">
         <span className="tag">PORTFÓLIO PROFISSIONAL</span>
@@ -36,6 +41,35 @@ function App() {
           </button>
         </div>
       </section>
+
+      {/* ABOUT SECTION */}
+
+      <section className="about">
+        <div className="about-text">
+
+          <span className="section-tag">
+            SOBRE MIM
+          </span>
+
+          <h2>
+            Estratégia, criatividade e comunicação.
+          </h2>
+
+          <p>
+            Sou um profissional de marketing e comunicação com experiência
+            em gestão de redes sociais, branding, copywriting e cobertura
+            de eventos institucionais.
+          </p>
+
+          <p>
+            Já trabalhei com plataformas como Facebook, Instagram,
+            WhatsApp e Twitter, ajudando marcas e projectos
+            a melhorarem a sua presença digital.
+          </p>
+
+        </div>
+      </section>
+
     </div>
   )
 }
